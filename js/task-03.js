fc09c62e57f3e0502c71aceb53a5132415d3547c
Alina Boiko <alina2boiko@gmail.com>
2021-10-17 20:06:40 +0300

    Create task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+let gallery = '';
+
+images.map(({ url, alt }) => {
+  gallery += `<li class="gallery--image-container"><img src="${url}" alt="${alt}" width="450px"></li>`;
+});
+
+const galleryElem = document.querySelector('.gallery');
+galleryElem.classList.add('gallery--align');
+galleryElem.insertAdjacentHTML('afterbegin', gallery);
